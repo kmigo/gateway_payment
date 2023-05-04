62e2c6ee26e0b5ddc6b5b75d594a3d223e450e57
psssDev <46029476+kmigo@users.noreply.github.com>
2023-05-04 12:42:47 -0300

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name="gateway_payment",
-    version="1.0.0",
+    version="0.7",
     packages=["gateway_payment"],
     include_package_data=True,
     description="Utilitários para gateways de pagamento",
