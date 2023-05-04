d4c737aeea510ce80de890aae87fb6f0f828218b
psssDev <46029476+kmigo@users.noreply.github.com>
2023-05-04 12:31:49 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 
 from setuptools import setup, find_packages
-
 setup(
     name="gateway_payment",
     version="1.0.0",
+    packages=["gateway_payment"],
+    include_package_data=True,
     description="Utilitários para gateways de pagamento",
     author="Patrick Soares",
     author_email="tk_patrick@hotmail.com",
-    packages=find_packages(),
-    package_dir={"": "."},
+    package_dir={"gateway_payment": ""},
     install_requires=[
         "requests",
-        "colorama",
-        "argparse>=1.4.0",
         "pagarme-python==4.0.2",
         "pydantic",
         "python-dotenv",
         "pytest"
 
     ],
-    entry_points={
-        "console_scripts": [
-            "gateway_payment=main:main"
-        ]
-    },
     python_requires=">=3.9.16",
 )
