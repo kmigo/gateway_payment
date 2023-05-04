1bd9c1c5d84ad64620e4040d02aca109126ac099
Joao Alves <71286115+Joao-Filh0@users.noreply.github.com>
2023-05-04 14:19:06 -0300

    v1.0.3

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup, find_packages
 setup(
     name="gateway-payment",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
-    package_dir={'': 'src'},
+    package_dir={'': 'src',"gateway_payment":"src/implementations/gateway/pagarmev5"},
     include_package_data=True,
     description="Utilitários para gateways de pagamento",
     author="Patrick Soares",
