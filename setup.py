95019245960d5fd336d1670ee06f77eb6ec8ae99
Joao Alves <71286115+Joao-Filh0@users.noreply.github.com>
2023-05-04 14:09:04 -0300

    import pagarmev5

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="gateway-payment",
     version="1.0.0",
-    packages=["gateway_payment.src.implementations.gateway"],
+    packages=["gateway_payment.src.implementations.gateway.pagarmev5","gateway_payment.src.implementations.gateway.pagarmev4"],
     include_package_data=True,
     description="Utilitários para gateways de pagamento",
     author="Patrick Soares",
     author_email="tk_patrick@hotmail.com",
-    package_dir={"gateway_payment": ""},
+    package_dir={"gateway_payment": "gateway_payment.src.implementations.gateway.pagarmev5"},
     install_requires=[
         "requests",
         "pagarme-python==4.0.2",
