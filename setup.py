9bdccad4d0bdec2f767c8bf9b6c837b11d969de2
Joao Alves <71286115+Joao-Filh0@users.noreply.github.com>
2023-05-04 14:29:06 -0300

    v1.0.6

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 setup(
     name="gateway-payment",
-    version="1.0.5",
-    packages=["gateway_payment.src.implementations.gateway.pagarmev5"],
+    version="1.0.6",
+    packages=["gateway_payment", "gateway_payment.src", "gateway_payment.src.implementations", "gateway_payment.src.implementations.gateway"],
     package_dir={"gateway_payment":""},
     include_package_data=True,
     description="Utilitários para gateways de pagamento",
