45c2c347aef48b9c6577e994339677e2eaef178b
psssDev <46029476+kmigo@users.noreply.github.com>
2023-05-04 15:24:32 -0300

    as

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="gateway-payment",
-    version="1.0.17",
+    version="1.0.18",
     packages=["gateway_payment",],
-    package_dir={"gateway_payment":"src/"},
+    package_dir={"gateway_payment":"src/gateway_payment"},
     include_package_data=True,
     description="Utilitários para gateways de pagamento",
     author="Patrick Soares",
