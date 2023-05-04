ee0408d1a2bcf94593afdcedba81fcb641c7454c
psssDev <46029476+kmigo@users.noreply.github.com>
2023-05-04 18:12:01 -0300

    ds

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gateway_payment",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages,
     package_dir={"gateway_payment":""},
     include_package_data=True,
